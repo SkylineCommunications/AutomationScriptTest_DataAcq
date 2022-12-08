b79198fb7aa9b4332225d2758cb0054e8d467ebc
Pedro Debevere <pedro.debevere@skyline.be>
2022-12-08 09:30:46 +0100

    added test project

---
+++ AutomationScript_ClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationScript_ClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AutomationScript_ClassLibrary")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("055B1D8C-4B20-4008-9575-B3DAC199A654")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ TestAutomation_2Tests/UtilityBisTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilitiesBis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesBis.Tests
+{
+	[TestClass()]
+	public class UtilityBisTests
+	{
+		[TestMethod()]
+		public void ToLowerTest()
+		{
+			var result = UtilitiesBis.UtilityBis.ToLower("B");
+
+			Assert.AreEqual("b", result);
+		}
+	}
+}
