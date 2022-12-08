9d09c5ea62adecc7f79138acf2fe830a5cdc526e
Pedro Debevere <pedro.debevere@skyline.be>
2022-12-08 12:27:20 +0100

    moved class

--- TestAutomation_2/TestAutomation_2.cs
+++ TestAutomation_2/TestAutomation_2.cs
@@ -69,14 +69,3 @@ public class Script
 
 	}
 }
-
-namespace UtilitiesBis
-{
-	public static class UtilityBis
-	{
-		public static string ToLower(string input)
-		{
-			return input.ToLower();
-		}
-	}
-}
